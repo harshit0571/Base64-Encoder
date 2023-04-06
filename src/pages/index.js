38b7baca9af76b34259f25d53a06c539846d1719
harshit0571 <harshitchadha65@gmail.com>
2023-04-06 21:30:54 +0530

    ui done

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [File, setFile] = useState(null);
-  const [Base64File, setBase64File] = useState("fdfd");
+  const [Base64File, setBase64File] = useState("");
   const Base64 = () => {
     const reader = new FileReader();
     const url = reader.readAsDataURL(File);
@@ -17,12 +17,14 @@ export default function Home() {
 
   return (
     <>
-      <div className="absolute flex-col justify-center items-center flex bg-slate-700 w-[100%] h-[100%]">
+      <div className="absolute flex-col justify-center items-center flex bg-slate-300 w-[100%] h-[100%]">
         <div className=" flex items-center flex-col">
-          <h1 className="lg:text-7xl sm:text-4xl sm:text-fuchsia-100 md:text-5xl text-yellow-300 gap-10">
+          <h1 className="lg:text-7xl sm:text-4xl md:text-5xl text-slate-700 gap-10">
             FILE ENCODER
           </h1>
-          <h1 className="text-xl text-yellow-300 mt-10 mb-5">enter file</h1>
+          <h1 className="text-xl text-slate-500 font-semibold mt-10 mb-5">
+            enter file
+          </h1>
         </div>
 
         <div className=" flex items-center flex-col">
@@ -33,10 +35,10 @@ export default function Home() {
           >
             ENCODE
           </button>
-          <h1 className="text-xl text-yellow-300 mt-10 mb-5">encoded text: </h1>
+          <h1 className="text-xl text-slate-950 mt-10 mb-5">encoded text: </h1>
           <textarea
             rows={10}
-            cols={20}
+            cols={30}
             readOnly
             value={Base64File}
             className="mt-7 text-lg "
@@ -45,6 +47,7 @@ export default function Home() {
             className="bg-white rounded-lg shadow-md text-2xl sm:text-lg text-red-500 p-2 mt-10"
             onClick={() => {
               navigator.clipboard.writeText(Base64File);
+              alert("copied");
             }}
           >
             COPY
